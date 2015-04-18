e97dd06945525a6502e8ca5e96446f3e858888d6
Fede Allocati <fede.allocati@gmail.com>
2015-04-18 16:17:24 -0300

    Automata Deterministico y No Deterministico con funcionalidad básica

--- TP1/src/automata.py
+++ TP1/src/automata.py
@@ -10,7 +10,27 @@ class AutomataDet(object):
 		self.Delta = { 'qT' : {}}
 		for simbolo in self.Sigma:
 			self.Delta['qT'][simbolo] = 'qT'
+	
+	def agregarEstado(estado):
+		if estado in self.Q
+			sys.exit("Se intento agregar el estado '%s' ya existente en el automata." % estado)
+		
+		self.Q.add(estado)
+		for simbolo in self.Sigma:
+			self.Delta[estado][simbolo] = 'qT'
 			
+	def agregarFinal(estado):
+		if not (estado in self.Q):
+			sys.exit("Se intento agregar a los estados finales el estado '%s', no existente en el automata." % estado)
+			
+		self.F.add(estado)
+			
+	def removerFinal(estado):
+		if not (estado in self.Q):
+			sys.exit("Se intento remover de los estados finales el estado '%s', no existente en el automata." % estado)
+			
+		self.F.remove(estado)
+		
 class AutomataNoDet(object):
 	def __init__(self):
 		self.Q = { 'qT' }
@@ -19,4 +39,24 @@ class AutomataNoDet(object):
 		self.q0 = 'qT'
 		self.Delta = { 'qT' : {}}
 		for simbolo in self.Sigma:
-			self.Delta['qT'][simbolo] = { 'qT' }
+			self.Delta['qT'][simbolo] = {}
+			
+	def agregarEstado(estado):
+		if (estado in self.Q)
+			sys.exit("Se intento agregar el estado '%s' ya existente en el automata." % estado)
+		
+		self.Q.add(estado)
+		for simbolo in self.Sigma:
+			self.Delta[estado][simbolo] = {}
+			
+	def agregarFinal(estado):
+		if not (estado in self.Q):
+			sys.exit("Se intento agregar a los estados finales el estado '%s', no existente en el automata." % estado)
+			
+		self.F.add(estado)
+			
+	def removerFinal(estado):
+		if not (estado in self.Q):
+			sys.exit("Se intento remover de los estados finales el estado '%s', no existente en el automata." % estado)
+			
+		self.F.remove(estado)
