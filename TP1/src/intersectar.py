2413cc3cf5968e48bc5cf82864d684ab4414e6cc
Sabrina <sizcovich@gmail.com>
2015-04-27 20:19:30 -0300

    arreglos de intersectar

--- TP1/src/intersectar.py
+++ TP1/src/intersectar.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*- 
+#!/usr/bin/python
+
 from automata import AutomataDet
 # A AND B = ¬(¬A U ¬B)
 def intersectar(automata1, automata2):
-	nuevo = AutomataDet(automata1 | automata2)
+
 	automata1Negado = construirComplemento(automata1)
 	automata2Negado = construirComplemento(automata2)
 
@@ -11,11 +14,11 @@ def intersectar(automata1, automata2):
 
 	return minimizar(nuevo)
 
-def union(automat1, automata2):
+def union(automata1, automata2):
 	for estado_aut1 in automata1.Q:
  		for estado_aut2 in automata2.Q:
  			nuevo.agregarEstado(estado_aut1 + estado_aut2)
- 			if estado_aut1 in automata1.F || estado_aut2 in automata2.F
+ 			if (estado_aut1 in automata1.F) or (estado_aut2 in automata2.F):
  				nuevo.agregarFinal(estado_aut1 + estado_aut2)
 
  	nuevo.setearInicial(automata1.q0 + automata2.q0)
