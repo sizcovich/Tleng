473fa023e7b9eff5364d21d46944348e26836961
Fede Allocati <fede.allocati@gmail.com>
2015-04-27 20:32:58 -0300

    Movido intersectar a construirInterseccion

--- TP1/src/construirDet.py
+++ TP1/src/construirDet.py
@@ -32,7 +32,32 @@ def construirComplemento(automata):
 	return renombrarEstados(nuevo)
 
 def construirInterseccion(automata1, automata2):
-	return AutomataDet("")
+	nuevo = AutomataDet(automata1 | automata2)
+	automata1Negado = construirComplemento(automata1)
+	automata2Negado = construirComplemento(automata2)
+
+	nuevo = construirUnion(automata1Negado, automata2Negado)
+	
+	nuevo = construirComplemento(nuevo)
+
+	return nuevo
+
+def construirUnion(automat1, automata2):
+	for estado_aut1 in automata1.Q:
+ 		for estado_aut2 in automata2.Q:
+ 			nuevo.agregarEstado(estado_aut1 + estado_aut2)
+ 			if estado_aut1 in automata1.F || estado_aut2 in automata2.F
+ 				nuevo.agregarFinal(estado_aut1 + estado_aut2)
+
+ 	nuevo.setearInicial(automata1.q0 + automata2.q0)
+
+	for estado1 in automata1.Q:
+		for estado2 in automata2.Q:
+			for simbolo in automata1.Delta[estado1]:
+				if simbolo in automata2.Delta[estado2]:
+					nuevo.setearArista(estado1 + estado2, simbolo, automata1.Delta[estado1][simbolo] + automata2.Delta[estado2][simbolo])
+
+	return renombrarEstados(nuevo)
 	
 def renombrarEstados(automata):
 	nuevo = AutomataDet(automata.Sigma)
