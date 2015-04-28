e8e52e4fd4a12965a99a4859db3cb44f2f3b1de6
Fede Allocati <fede.allocati@gmail.com>
2015-04-27 21:40:34 -0300

    Intersectar como Union

--- TP1/src/construirDet.py
+++ TP1/src/construirDet.py
@@ -35,7 +35,24 @@ def construirComplemento(automata):
 	return renombrarEstadosDet(nuevo)
 
 def construirInterseccion(automata1, automata2):
-	return construirComplemento(construirUnion(construirComplemento(automata1), construirComplemento(automata2)))
+	nuevo = AutomataDet(automata1.Sigma | automata2.Sigma)
+
+	for estado_aut1 in automata1.Q:
+ 		for estado_aut2 in automata2.Q:
+ 			nuevo.agregarEstado(estado_aut1 + '-' + estado_aut2)
+
+ 			if (estado_aut1 in automata1.F) and (estado_aut2 in automata2.F):
+ 				nuevo.agregarFinal(estado_aut1 + '-' + estado_aut2)
+
+ 	nuevo.setearInicial(automata1.q0 + automata2.q0)
+
+	for estado1 in automata1.Q:
+		for estado2 in automata2.Q:
+			for simbolo in automata1.Delta[estado1]:
+				if simbolo in automata2.Delta[estado2]:
+					nuevo.setearArista(estado1 + '-' + estado2, simbolo, automata1.Delta[estado1][simbolo] + '-' + automata2.Delta[estado2][simbolo])
+
+	return renombrarEstadosDet(nuevo)
 
 def construirUnion(automata1, automata2):
 	nuevo = AutomataDet(automata1.Sigma | automata2.Sigma)
