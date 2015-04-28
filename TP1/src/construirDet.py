7e15317c45b5f42bd990ef2e50bfbfb0ffa8496c
Fede Allocati <fede.allocati@gmail.com>
2015-04-27 22:14:26 -0300

    Una cosita en intersectar

--- TP1/src/construirDet.py
+++ TP1/src/construirDet.py
@@ -12,7 +12,7 @@ def construirComplemento(automata):
 		if not estado in automata.F:
 			nuevo.agregarFinal(estado)
 
-	# Mando todos los que no esten definidos al trampa
+	# mando todos los que no esten definidos al trampa
 	agregarTrampa = False
 	for estado in automata.Q:
 		if Set(automata.Delta[estado].keys()) != automata.Sigma:
@@ -27,7 +27,7 @@ def construirComplemento(automata):
 
 	for estado in nuevo.Q:
 		for simbolo in nuevo.Sigma:
-			if estado != 'qT' and simbolo in automata.Delta[estado]:
+			if simbolo in automata.Delta[estado]:
 					nuevo.setearArista(estado, simbolo, automata.Delta[estado][simbolo])
 			else:
 				nuevo.Delta[estado][simbolo] = 'qT'
@@ -44,7 +44,7 @@ def construirInterseccion(automata1, automata2):
  			if (estado_aut1 in automata1.F) and (estado_aut2 in automata2.F):
  				nuevo.agregarFinal(estado_aut1 + '-' + estado_aut2)
 
- 	nuevo.setearInicial(automata1.q0 + '-' + automata2.q0)
+ 	nuevo.setearInicial(automata1.q0 + '-' + automata2.q0)	
 
 	for estado1 in automata1.Q:
 		for estado2 in automata2.Q:
@@ -52,25 +52,19 @@ def construirInterseccion(automata1, automata2):
 				if simbolo in automata2.Delta[estado2]:
 					nuevo.setearArista(estado1 + '-' + estado2, simbolo, automata1.Delta[estado1][simbolo] + '-' + automata2.Delta[estado2][simbolo])
 
-	return renombrarEstadosDet(nuevo)
+	# mando todos los que no esten definidos al trampa
+	agregarTrampa = False
+	for estado in automataDet.Q:
+		if Set(automataDet.Delta[estado].keys()) != automataDet.Sigma:
+			agregarTrampa = True
+			break
 
-def construirUnion(automata1, automata2):
-	nuevo = AutomataDet(automata1.Sigma | automata2.Sigma)
-
-	for estado_aut1 in automata1.Q:
- 		for estado_aut2 in automata2.Q:
- 			nuevo.agregarEstado(estado_aut1 + '-' + estado_aut2)
-
- 			if (estado_aut1 in automata1.F) or (estado_aut2 in automata2.F):
- 				nuevo.agregarFinal(estado_aut1 + '-' + estado_aut2)
-
- 	nuevo.setearInicial(automata1.q0 + '-' + automata2.q0)
-
-	for estado1 in automata1.Q:
-		for estado2 in automata2.Q:
-			for simbolo in automata1.Delta[estado1]:
-				if simbolo in automata2.Delta[estado2]:
-					nuevo.setearArista(estado1 + '-' + estado2, simbolo, automata1.Delta[estado1][simbolo] + '-' + automata2.Delta[estado2][simbolo])
+	if agregarTrampa:
+		nuevo.agregarEstado('qT')
+		for estado in nuevo.Q:
+			for simbolo in nuevo.Sigma:
+				if not (simbolo in nuevo.Delta[estado]):
+					automataDet.Delta[estado][simbolo] = 'qT'
 
 	return renombrarEstadosDet(nuevo)
 	
